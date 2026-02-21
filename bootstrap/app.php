bf98fbaadf179673b2fe15a7c4a9651ecf7041e4
naidrahiqa <akufaqih1@gmail.com>
2026-02-21 23:52:22 +0700

    fix(vercel): Override write storage paths for Vercel's readonly filesystem

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,7 +4,7 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
-return Application::configure(basePath: dirname(__DIR__))
+$app = Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
         commands: __DIR__.'/../routes/console.php',
@@ -17,3 +17,10 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withExceptions(function (Exceptions $exceptions): void {
         //
     })->create();
+
+// Override storage path if running on Vercel Serverless Function
+if (isset($_ENV['VERCEL']) || env('VERCEL_ENV')) {
+    $app->useStoragePath('/tmp/storage');
+}
+
+return $app;
